e5ee64a1b296f20c58d966870abb7a8a3093d972
lalithbseervi <lcoding20@gmail.com>
2025-07-11 16:27:24 +0530

    fix: add missing `NOT NULL` checks

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -13,16 +13,16 @@ CREATE TABLE organisers (
 CREATE TABLE events (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT NOT NULL CHECK(LENGTH(name) <= 128),
-  date DATETIME NOT NULL,
+  date DATETIME NOT NULL
 );
 
 CREATE TABLE teams (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   event_id INTEGER NOT NULL,
   name TEXT NOT NULL CHECK(LENGTH(name) <= 128), 
-  lead_name TEXT NOT NULL CHECK(LENGTH(lead_name) <= 128)),
-  lead_email TEXT CHECK(LENGTH(lead_email) <= 256),
-  num_participants INTEGER CHECK(num_participants <= 10),
+  lead_name TEXT NOT NULL CHECK(LENGTH(lead_name) <= 128),
+  lead_email TEXT NOT NULL CHECK(LENGTH(lead_email) <= 256),
+  num_participants INTEGER NOT NULL CHECK(num_participants <= 10),
   FOREIGN KEY (event_id) REFERENCES events(id) ON DELETE CASCADE
 );
 
@@ -33,11 +33,11 @@ CREATE TABLE participants (
   email TEXT NOT NULL CHECK(LENGTH(email) <= 256),
   phone TEXT NOT NULL CHECK(LENGTH(phone) = 10 AND phone GLOB '[0-9]*'),
   college TEXT NOT NULL CHECK(LENGTH(college) <= 256),
-  srn TEXT CHECK(LENGTH(srn) <= 32),
-  branch TEXT CHECK(LENGTH(branch) <= 2),
-  day_scholar BOOLEAN,
+  srn TEXT NOT NULL CHECK(LENGTH(srn) <= 32),
+  branch TEXT NOT NULL CHECK(LENGTH(branch) <= 4),
+  day_scholar INTEGER NOT NULL CHECK(day_scholar IN (0, 1)),
   hostel TEXT CHECK(LENGTH(hostel) <= 8),
-  qr_string TEXT NOT NULL UNIQUE CHECK(LENGTH(qr_string) <= 256),
+  qr_string TEXT NOT NULL UNIQUE,
   FOREIGN KEY (team_id) REFERENCES teams(id) ON DELETE CASCADE
 );
 
