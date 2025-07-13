6aacc01a4ba89210aac2d82b5986d906481eed88
lalithbseervi <lcoding20@gmail.com>
2025-07-14 01:09:42 +0530

    feat: auto-check whether an admin exists

---
+++ backend/db/utils/checkAdminRecords.js
@@ -0,0 +1,14 @@
+import selectFromTable from './select.js';
+import promptAdminCreation from './execute.js';
+
+async function checkAdminRecords() {
+    let result = await selectFromTable('dbAuthorisedUsers', ['role'], ['admin']);
+
+    if(!result) {
+        console.log('No admin found in the db.');
+        await promptAdminCreation();
+    }
+    else console.log(`${result.length} admins exist. no need for prompting admin creation`);
+}
+
+export default checkAdminRecords;
--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import routes from './routes/index.js';
+import checkAdminRecords from './db/utils/checkAdminRecords.js';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+checkAdminRecords();
 
 const PORT = process.env.PORT ? process.env.PORT : 8000;
 
